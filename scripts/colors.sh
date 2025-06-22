d56aac261597472f7f94a41d5bf293deb9eb4a6b
Rodrigo Nascimento <rodrigo.nascimento@edu.udesc.br>
2025-06-21 22:20:54 -0300

    fix: path to color script

---
+++ scripts/colors.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+GREEN=$(tput setaf 2 2>/dev/null || echo "")
+YELLOW=$(tput setaf 3 2>/dev/null || echo "")
+BLUE=$(tput setaf 4 2>/dev/null || echo "")
+CYAN=$(tput setaf 6 2>/dev/null || echo "")
+RESET=$(tput sgr0 2>/dev/null || echo "")
