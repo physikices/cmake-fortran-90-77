55c40576e6946ce53af5a73900b0e0d68326b457
Rodrigo Nascimento <rodrigo.nascimento@edu.udesc.br>
2025-07-12 18:08:03 -0300

    feat: improve clean snippet

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -2,5 +2,6 @@
 GREEN=$(tput setaf 2 2>/dev/null || echo "")
 YELLOW=$(tput setaf 3 2>/dev/null || echo "")
 BLUE=$(tput setaf 4 2>/dev/null || echo "")
+RED=$(tput setaf 1 2>/dev/null || echo "")
 CYAN=$(tput setaf 6 2>/dev/null || echo "")
 RESET=$(tput sgr0 2>/dev/null || echo "")
